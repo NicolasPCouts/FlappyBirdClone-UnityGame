8c5518874350684de875147cd067cd66f1cdfdd6
NicolasPCouts <nicolaspereiracoutinho@gmail.com>
2018-07-14 21:25:14 -0300

    Final Version

---
+++ Scripts/DeadCanvas_Manager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeadCanvas_Manager : MonoBehaviour
+{
+    public Text currentScore;
+    public Text bestScore;
+
+    public void SetScoreValues(int currentSc, int bestSc)
+    {
+        currentScore.text = currentSc.ToString();
+        bestScore.text = bestSc.ToString();
+    }
+}
---
+++ Scripts/PlayAudioOnTriggerEnter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioOnTriggerEnter : MonoBehaviour
+{
+    public AudioSource source;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (!collision.gameObject.GetComponent<FlappyBirdController>().IsDead)
+                source.Play();
+        }
+    }
+}
